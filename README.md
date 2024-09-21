1fdf8de0c85399c85d70c65076072b151b215b7b
Tanvi Diwakar <tanvidiwakar04@gmail.com>
2024-09-22 00:45:27 +0530

    Update and rename README.md to calculator.cpp

--- README.md
+++
@@ -1,2 +0,0 @@
-# CODSOFT
-Hello ... this is my first project as c++ intern at codsoft it was great experience creating this amazing project 
---
+++ calculator.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    char op;
+
+    cout << " enter the operator (+ - * /) : ";
+    cin >> op;
+
+    cout <<"enter the numbers one after another : ";
+    cin >> num1 >> num2;
+
+
+    switch (op)
+    {
+        case '+':
+        cout << num1 << op << num2 <<"=" << num1 + num2 << endl;
+        break;
+
+        case '-':
+        cout << num1 << op << num2 <<"=" << num1 - num2 << endl;
+        break;
+
+        case '*':
+        cout << num1 << op << num2 <<"=" << num1 * num2 << endl;
+        break;
+        
+
+        case '/':
+        if( num2 ==0.0)
+        {
+            cout << "divide by zero situation" << endl;
+        }
+        else
+        {
+            cout << num1 << op << num2 <<"=" << num1 / num2 << endl;
+        }
+        break;
+
+    default:    
+    cout << "invalid operator " << endl;
+    }
+}
